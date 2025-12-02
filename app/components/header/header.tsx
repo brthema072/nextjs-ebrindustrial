e367308be9c59536f16e81a9b40638cedf280ec5
brthema072 <brunomarthe@hotmail.com>
2025-12-01 22:25:42 -0300

    Feat: Header component and logo

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,7 +1,16 @@
 const Header = () => {
   return (
     <>
-      <span>Header</span>
+      <div className="flex flex-row items-center justify-around w-full px-12 py-6">
+        <img src="/images/logo.png" alt="Logo" width={135} height={57} />
+
+        <div className="flex flex-row items-center gap-6 text-[#2165E9]">
+          <span className="cursor-pointer">Home</span>
+          <span className="cursor-pointer">Equipamentos</span>
+          <span className="cursor-pointer">Montagem industriais</span>
+          <span className="cursor-pointer">Contato e orçamentos</span>
+        </div>
+      </div>
     </>
   )
 }

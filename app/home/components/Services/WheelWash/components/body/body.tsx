c5686a20a5961125133836807176c7c0bdda28f4
brthema072 <brunomarthe@hotmail.com>
2025-12-06 23:17:37 -0300

    Feat: Wheel wash header and component

---
+++ app/home/components/Services/WheelWash/components/body/body.tsx
@@ -0,0 +1,86 @@
+import Link from 'next/link'
+
+const Body = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full h-auto inset-0">
+        <div className="w-[40%] text-center inset-0 pt-8 pb-4">
+          <h1 className="text-[2.5rem] font-bold text-[#232323] md:text-4xl">
+            Versátil com o melhor custo-benefício do mercado
+          </h1>
+        </div>
+
+        <div className="w-[50%] mt-2 border-b border-gray-200"></div>
+
+        <div className="flex flex-col items-stretch justify-center w-[40%] gap-4 mt-8">
+          <span className="text-left">
+            O <span className="font-bold">Lava Rodas EBR1296</span> tem uma capacidade de segregação
+            de resíduos excepcional, garantindo uma limpeza eficiente e sustentável. Com manutenções
+            periódicas, o equipamento tem uma vida útil prolongada, proporcionando confiabilidade e
+            durabilidade.
+          </span>
+
+          <span className="text-left">
+            O equipamento é composto por duas partes: uma estrutura monobloco por onde passam os
+            veículos e um tanque de reciclo.
+          </span>
+
+          <span className="text-left">
+            Sua mobilidade e montagem podem ser feita sem a necessidade de recursos dispendiosos,
+            dispensando totalmente qualquer tipo de obra civil; O equipamento pode ser colocado
+            sobre qualquer tipo de solo, sem necessidade de escavações ou tempo adicional para
+            preparação.
+          </span>
+
+          <Link
+            href="/wheel-wash"
+            target="_blank"
+            className="text-[1.37rem] text-[#0D3AEC] font-bold underline underline-offset-2 cursor-pointer"
+          >
+            Saiba mais sobre o Lava Rodas EBR1296 {'>'}
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-3 gap-6 mt-16">
+          <Link href="/wheel-wash" target="_blank">
+            <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+              <h3 className="text-xl font-bold">Foto</h3>
+            </div>
+          </Link>
+
+          <Link href="/wheel-wash" target="_blank">
+            <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+              <h3 className="text-xl font-bold">Foto</h3>
+            </div>
+          </Link>
+
+          <Link href="/wheel-wash" target="_blank">
+            <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+              <h3 className="text-xl font-bold">Foto</h3>
+            </div>
+          </Link>
+
+          <Link href="/wheel-wash" target="_blank">
+            <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+              <h3 className="text-xl font-bold">Foto</h3>
+            </div>
+          </Link>
+
+          <Link href="/wheel-wash" target="_blank">
+            <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+              <h3 className="text-xl font-bold">Foto</h3>
+            </div>
+          </Link>
+
+          <Link href="/wheel-wash" target="_blank">
+            <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+              <h3 className="text-xl font-bold">Foto</h3>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Body
---
+++ app/home/components/Services/WheelWash/components/header/header.tsx
@@ -0,0 +1,28 @@
+const Header = () => {
+  return (
+    <>
+      <div className="relative h-[420px] md:h-[337px] bg-cover bg-center">
+        <div className="absolute inset-0 bg-[url('/images/bg-home.png')] bg-cover bg-center bg-fixed"></div>
+
+        <div className="absolute inset-0 bg-[#3E556F] opacity-80"></div>
+
+        <div className="absolute inset-0 bg-white clip-left-bottom"></div>
+        <div className="absolute inset-0 bg-white clip-right-bottom"></div>
+
+        <div className="relative z-10 w-full mx-auto  h-full flex flex-col items-center justify-center">
+          <h1 className="text-7xl font-bold text-yellow-400">Lava Rodas EBR1296</h1>
+
+          <div className="w-180">
+            <h2 className="text-xl md:text-3xl font-semibold text-white mt-4 leading-normal text-center">
+              Lavagem e segregação de resíduos com{' '}
+              <span className="bg-[#008AFC] text-white px-2 py-1 rounded">economia de 90%</span>
+              de água utilizada
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header

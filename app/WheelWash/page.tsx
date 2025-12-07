58e3ed428c6bac50e6842300c6c8b52050090b85
brthema072 <brunomarthe@hotmail.com>
2025-12-06 23:18:08 -0300

    Feat: Wheel wash page

---
+++ app/WheelWash/page.tsx
@@ -0,0 +1,11 @@
+const WheelWash = () => {
+  return (
+    <>
+      <div className="flex flex-row items-center justify-center w-full h-[318px] pt-[105px] bg-[#3d5975]">
+        <h1 className="text-5xl font-bold text-white text-center">LAVA RODAS EBR1296</h1>
+      </div>
+    </>
+  )
+}
+
+export default WheelWash

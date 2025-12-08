16f9054e482009807eda0490382df0b58bb30066
brthema072 <brunomarthe@hotmail.com>
2025-12-07 22:48:50 -0300

    Feat: Our clients page

---
+++ app/home/components/Services/OurClinets/OurClients.tsx
@@ -0,0 +1,29 @@
+const OurClients = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full h-auto py-12">
+        <h1 className="text-2xl text-[#1e2d3b] font-semibold">NOSSOS CLIENTES</h1>
+
+        <div className="flex flex-row items-center justify-evenly w-[80%] h-auto mt-24">
+          <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+            <h3 className="text-xl font-bold">Logo</h3>
+          </div>
+
+          <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+            <h3 className="text-xl font-bold">Logo</h3>
+          </div>
+
+          <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+            <h3 className="text-xl font-bold">Logo</h3>
+          </div>
+
+          <div className="bg-blue-500 text-white rounded-lg shadow w-[320px] h-[180px]">
+            <h3 className="text-xl font-bold">Logo</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default OurClients

dd3be132fcc73ff4f8bc7fda03c58d87d8067db7
brthema072 <brunomarthe@hotmail.com>
2025-12-13 11:11:03 -0300

    Feat: Header wheel wash page

--- app/wheel-wash/page.tsx
+++ app/wheel-wash/page.tsx
@@ -1,8 +1,13 @@
 const WheelWash = () => {
   return (
     <>
-      <div className="flex flex-row items-center justify-center w-full h-[318px] pt-[105px] bg-[#3d5975]">
-        <h1 className="text-5xl font-bold text-white text-center">LAVA RODAS EBR1296</h1>
+      <div className="pt-[105px] bg-[#F7F7F7]">
+        <div className="flex flex-row items-center justify-center relative w-full h-[400px] bg-[#3d5975]">
+          <div className="absolute inset-0 m-auto bg-[url('/images/bg-home.png')] bg-cover bg-center w-[98%] my-4 rounded-sm"></div>
+          <div className="absolute inset-0 bg-[rgb(5,27,55)] opacity-20"></div>
+
+          <h1 className="relative text-4xl font-bold text-white text-center">LAVA RODAS EBR1296</h1>
+        </div>
       </div>
     </>
   )

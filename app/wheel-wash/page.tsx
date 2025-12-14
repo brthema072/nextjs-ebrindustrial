13f5571aa4e94e25f72a9cdafb3626ee6f7cec65
brthema072 <brunomarthe@hotmail.com>
2025-12-13 23:39:56 -0300

    Feat: Wheel wash body page

--- app/wheel-wash/page.tsx
+++ app/wheel-wash/page.tsx
@@ -8,6 +8,39 @@ const WheelWash = () => {
 
           <h1 className="relative text-4xl font-bold text-white text-center">LAVA RODAS EBR1296</h1>
         </div>
+
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 py-10">
+          <div className="flex flex-row items-center justify-center w-full">
+            <img
+              src="/images/bg-home.png"
+              className="bg-cover rounded-sm shadow-md shadow-gray-400"
+              width={528}
+              height={396}
+            />
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-[80%] h-auto gap-4">
+            <span className="text-start">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </span>
+
+            <span className="text-start">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </span>
+          </div>
+        </div>
       </div>
     </>
   )

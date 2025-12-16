2e54b136c3c22c3b53987823bac6738d25a6808b
brthema072 <brunomarthe@hotmail.com>
2025-12-16 09:43:36 -0300

    Style: Mobile heaader

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { useState, useRef, useEffect } from 'react'
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
   const [open, setOpen] = useState(false)
   const ref = useRef<HTMLDivElement>(null)
 
@@ -20,57 +21,72 @@ const Header = () => {
 
   return (
     <>
-      <div className="fixed flex flex-row items-center justify-around w-full px-12 py-6 bg-white z-50">
+      <div className="fixed flex flex-row items-start md:items-center justify-around w-full px-12 py-6 bg-white z-50">
         <img src="/images/logo.png" alt="Logo" width={135} height={57} />
 
-        <div className="flex flex-row items-center gap-6 text-[#2165E9]">
-          <Link href="/home">Home</Link>
-          <Link href="/equipment">Equipamentos</Link>
-          <div
-            className="relative cursor-pointer select-none"
-            ref={ref}
-            onClick={() => setOpen(!open)}
+        <div className="flex flex-col items-end md:items-center">
+          <button
+            className="md:hidden text-[#2165E9] text-4xl"
+            onClick={() => setMenuOpen(!menuOpen)}
           >
-            <span>Montagem industriais</span>
-            <div
-              className={`absolute mt-2 w-56 bg-white border border-gray-200 rounded-md shadow-lg transition-all duration-200 origin-top ${
-                open ? 'scale-100 opacity-100' : 'scale-95 opacity-0 pointer-events-none'
-              }`}
-            >
-              <ul className="py-2">
-                <li>
-                  <a href="/rotary-sieve" className="block px-4 py-2 hover:bg-gray-100">
-                    Peneira Rotativa (TROMMEL)
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/construction-demolition-wast-recycling"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Usina Reciclagem Entulho RCC
-                  </a>
-                </li>
-                <li>
-                  <a href="/crusher" className="block px-4 py-2 hover:bg-gray-100">
-                    Britadores
-                  </a>
-                </li>
-                <li>
-                  <a href="/wheel-wash" className="block px-4 py-2 hover:bg-gray-100">
-                    Lava Rodas EBR1296
-                  </a>
-                </li>
-                <li>
-                  <a href="/metal-structure" className="block px-4 py-2 hover:bg-gray-100">
-                    Estruturas Metálicas
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+            ☰
+          </button>
 
-          <Link href="/quote">Contato e orçamentos</Link>
+          <div
+            className={`${
+              menuOpen ? 'flex' : 'hidden'
+            } flex flex-col md:flex-row items-end md:items-center h-screen md:h-auto gap-6 text-[#2165E9] text-end md:text-center`}
+          >
+            <Link href="/home">Home</Link>
+            <Link href="/equipment">Equipamentos</Link>
+            <div
+              className="relative cursor-pointer select-none"
+              ref={ref}
+              onClick={() => setOpen(!open)}
+            >
+              <span>Montagem industriais</span>
+              <div
+                className={`md:absolute mt-2 w-56 bg-white border border-gray-200 rounded-md shadow-lg transition-all duration-200 origin-top ${
+                  open
+                    ? 'scale-100 opacity-100 block'
+                    : 'scale-95 opacity-0 pointer-events-none hidden'
+                }`}
+              >
+                <ul className="py-2">
+                  <li>
+                    <a href="/rotary-sieve" className="block px-4 py-2 hover:bg-gray-100">
+                      Peneira Rotativa (TROMMEL)
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/construction-demolition-wast-recycling"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Usina Reciclagem Entulho RCC
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/crusher" className="block px-4 py-2 hover:bg-gray-100">
+                      Britadores
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/wheel-wash" className="block px-4 py-2 hover:bg-gray-100">
+                      Lava Rodas EBR1296
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/metal-structure" className="block px-4 py-2 hover:bg-gray-100">
+                      Estruturas Metálicas
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <Link href="/quote">Contato e orçamentos</Link>
+          </div>
         </div>
       </div>
     </>

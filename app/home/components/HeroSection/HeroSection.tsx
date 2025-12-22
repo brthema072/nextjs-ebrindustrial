37e1a1de2876f6cfe8199f9b39d26764718ea9e7
brthema072 <brunomarthe@hotmail.com>
2025-12-22 08:56:35 -0300

    Style: Mobile font size hero section

--- app/home/components/HeroSection/HeroSection.tsx
+++ app/home/components/HeroSection/HeroSection.tsx
@@ -7,12 +7,12 @@ const HeroSection = () => {
         <div className="absolute inset-0 bg-[rgb(5,27,55)] opacity-80"></div>
 
         <div className="relative w-full h-full flex flex-col items-center justify-start p-10 pt-20">
-          <h1 className="text-white text-4xl max-w-4xl text-center font-bold leading-normal">
+          <h1 className="text-white text-2xl md:text-4xl max-w-4xl text-center font-bold leading-normal">
             Soluções inovadoras para montagens industriais, caldeiraria, usinagem e equipamentos
             ecológicos.
           </h1>
 
-          <h3 className="text-gray-300 text-2xl max-w-3xl text-center mt-20">
+          <h3 className="text-gray-300 text-lg md:text-2xl max-w-3xl text-center mt-20">
             Com mais de uma década de experiência, a
             <span className="font-bold text-white">EBR Industrial</span> fornece serviços de alta
             qualidade para a indústria brasileira
